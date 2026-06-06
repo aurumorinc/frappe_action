2da3836e9242413d60b13939bc12eda8b5256e88
Aryan Singh <aquiveal@gmail.com>
2026-06-07 03:35:10 +0530

    fix: improve error message for missing action
    Update the error message text to be more descriptive and user-friendly when an action document is not found.

--- frappe_orbit/action.py
+++ frappe_orbit/action.py
@@ -4,7 +4,7 @@ from frappe import _
 @frappe.whitelist()
 def get(name):
     if not frappe.db.exists("Action", name):
-        frappe.throw(_("Action {0} not found").format(name))
+        frappe.throw(_("The requested action '{0}' could not be found.").format(name))
         
     action = frappe.get_doc("Action", name)
     return action.as_dict()
