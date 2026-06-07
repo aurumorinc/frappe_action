ec775d27837e1e3523517a80fe4b08ba53afa5b1
Aryan Singh <aquiveal@gmail.com>
2026-06-07 22:38:14 +0530

    feat: update todo module functionality
    Refactor the todo module to support structured task management. Update the retrieval logic to return lists of open and closed tasks, implement report generation for user productivity metrics, and add support for hierarchical sub-tasks via a main reference field.
    
    Also, add permission checks in the trigger_sub method to ensure only assigned users or administrators can modify specific tasks.

--- frappe_orbit/todo.py
+++ frappe_orbit/todo.py
@@ -3,29 +3,88 @@ import frappe
 from frappe import _
 
 @frappe.whitelist()
-def get_open():
+def get_open(limit=9):
     user = frappe.session.user
     
-    # Find the oldest open ToDo assigned to the user that has an Action
-    todos = frappe.get_all(
+    # Fetch up to `limit` open ToDos (ordered by priority desc, creation asc) where `main` is null or empty
+    open_todos = frappe.get_all(
         "ToDo",
         filters={
             "allocated_to": user,
             "status": "Open",
-            "action": ["is", "set"]
+            "main": ["in", ["", None]]
         },
-        order_by="creation asc",
+        fields=["*"],
+        order_by="priority desc, creation asc",
+        limit=limit
+    )
+    
+    # Fetch 1 most recently closed ToDo where `main` is null or empty
+    recent_closed = frappe.get_all(
+        "ToDo",
+        filters={
+            "allocated_to": user,
+            "status": ["in", ["Closed", "Cancelled"]],
+            "main": ["in", ["", None]]
+        },
+        fields=["*"],
+        order_by="modified desc",
         limit=1
     )
     
-    if todos:
-        todo = frappe.get_doc("ToDo", todos[0].name)
-        return todo.as_dict()
-            
-    return None
+    return {
+        "open_todos": open_todos,
+        "recent_closed": recent_closed
+    }
 
 @frappe.whitelist()
-def submit(doc):
+def get_report():
+    user = frappe.session.user
+    
+    # Calculate total_open: Count of all open ToDos assigned to the user
+    total_open = frappe.db.count(
+        "ToDo",
+        filters={
+            "allocated_to": user,
+            "status": "Open"
+        }
+    )
+    
+    # Calculate completed_today: Count of all closed/cancelled ToDos assigned to the user, modified today
+    today = frappe.utils.nowdate()
+    completed_today = frappe.db.count(
+        "ToDo",
+        filters={
+            "allocated_to": user,
+            "status": ["in", ["Closed", "Cancelled"]],
+            "modified": [">=", today]
+        }
+    )
+    
+    return {
+        "total_open": total_open,
+        "completed_today": completed_today
+    }
+
+@frappe.whitelist()
+def get_sub(parent_id):
+    user = frappe.session.user
+    
+    # Fetch all ToDos where `main` equals `parent_id`
+    sub_todos = frappe.get_all(
+        "ToDo",
+        filters={
+            "allocated_to": user,
+            "main": parent_id
+        },
+        fields=["*"],
+        order_by="priority desc, creation asc"
+    )
+    
+    return sub_todos
+
+@frappe.whitelist()
+def save(doc):
     if isinstance(doc, str):
         doc = json.loads(doc)
         
@@ -44,8 +103,11 @@ def submit(doc):
 def trigger_sub(parent_todo_id, action_name, context_data=None):
     parent_todo = frappe.get_doc("ToDo", parent_todo_id)
     
+    if parent_todo.allocated_to != frappe.session.user and frappe.session.user != "Administrator":
+        frappe.throw(_("You do not have permission to modify this ToDo. It is allocated to another user."), frappe.PermissionError)
+    
     if not frappe.db.exists("Action", action_name):
-        frappe.throw(_("Action {0} not found").format(action_name))
+        frappe.throw(_("The requested action '{0}' could not be found.").format(action_name))
         
     new_todo = frappe.get_doc({
         "doctype": "ToDo",
@@ -54,7 +116,7 @@ def trigger_sub(parent_todo_id, action_name, context_data=None):
         "reference_type": parent_todo.reference_type,
         "reference_name": parent_todo.reference_name,
         "description": _("Sub-todo of {0}").format(parent_todo_id),
-        "depends_on": parent_todo_id
+        "main": parent_todo_id
     })
     
     new_todo.insert(ignore_permissions=True)
